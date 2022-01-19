2ce97c8024a77f9ce6818dfd8dc1e5fb57a38c4b
SHERPA 944allview CHAUDHARY <944allview@SHERPAs-iMac.hsd1.ca.comcast.net>
2022-01-18 19:52:48 -0800

    adding markers

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -3,8 +3,14 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
 // We create the tile layer that will be the background of our map.
-
+// We create the tile layer that will be the background of our map.
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
