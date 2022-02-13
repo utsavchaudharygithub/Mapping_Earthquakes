f353e6e69e6ba844f85b1fa96a0669173af63bfb
SHERPA 944allview CHAUDHARY <944allview@SHERPAs-iMac.hsd1.ca.comcast.net>
2022-02-12 22:55:26 -0800

    adding files

---
+++ Earthquake_challenge/static/js/config.js
@@ -0,0 +1,1 @@
+API_KEY= "pk.eyJ1IjoidXRzYXZjaGF1ZGhhcnkiLCJhIjoiY2t5anB3dTFmMmlzajJ2cGk4NmlmendseSJ9.HDe4VFYZ8Occ0LXapGBS3Q";
--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,107 +1,24 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
+
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
-// Coordinates for each point to be used in the line.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
-//  Add a marker to the map for Los Angeles, California.
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
+//let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-//L.circle([34.0522, -118.2437], {
-    //radius: 300,
-    //color: "black",
-    //fillcolor: "#ffffa1"
-// An array containing each city's location, state, and population.
-// Get data from cities.js
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-
-
-let cityData = cities;
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-  }
-  ];
-  //addTo(map);
- 
-  // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-    L.marker(city.location).addTo(map);
-    console.log(city)
-   });
- radius: city.population/100000
-
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
-L.circle([34.0522, -118.2437], {
-    radius: 100
- }).addTo(map);
- 
- //radius: city.population/100000
-
-
- //Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.marker(city.location)
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-   .addTo(map);
-});
-
-
-// Loop through the cities array and create one marker for each city.
-//cityData.forEach(function(city) {
-    //console.log(city)
-    //L.marker(city.location).addTo(map);
-//});
-
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  });
 
 // We create the tile layer that will be the background of our map.
-//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
+// We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    id: 'mapbox.streets',
     accessToken: API_KEY
 });
-
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
